dafd2f9d3e358681651ac8632666551564382cc9
Evgeny Kucherenko <evg.kch@gmail.com>
2021-08-13 16:45:36 +0300

    fix: parsePath function fixed

--- helpers.js
+++ helpers.js
@@ -4,11 +4,21 @@ const parsePath = p => {
 		
 	p = path.parse(p);
 
-	if (p.name.length === 0)
-		p.name = 'index';
-
 	if (p.ext.length === 0)
-		p.ext = p.dir === '/' ? '.html' : '.js'
+	{
+		if (p.name.length === 0)
+		{
+			p.ext = '.html';
+			p.name = 'index';			
+		}
+		else
+		{
+			p.dir += p.name;
+			p.ext = '.js';
+			p.name = 'index';			
+		}
+		p.base = p.name + p.ext;
+	}
 		
 	p = path.join(path.resolve(), path.format(p));
 	p = path.parse(p);
