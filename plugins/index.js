5d1dbdd736e3a6cee7a51db281380157373db134
Evgeny Kucherenko <evg.kch@gmail.com>
2022-09-26 13:05:43 +0300

    bug fixes

--- plugins/index.js
+++ plugins/index.js
@@ -1,5 +1,4 @@
 module.exports = {
     FileWatcher: require('./watcher/index.js'),
-    Module: require('./module/index.js'),
-    JS: require('./js/index.js')
+    Module: require('./module/index.js')
 };
